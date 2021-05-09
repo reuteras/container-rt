c673e0eefa45cb0a27b7abd353c46b7d7773b771
Peter Reuterås <peter@reuteras.com>
2021-05-09 20:03:25 +0200

    Only print IP address...

--- rt/entrypoint.sh
+++ rt/entrypoint.sh
@@ -41,10 +41,10 @@ while [[ $EXT_IP == "" ]] ; do
     EXT_IP=$(curl -s https://icanhazip.com)
 done
 
-DNS_IP=$(dig @8.8.8.8 "$RT_HOSTNAME" | grep -v ';' | grep -v CNAME | grep A)
+DNS_IP=$(dig @8.8.8.8 "$RT_HOSTNAME" | grep -v ';' | grep -v CNAME | grep A | awk '{print $5}')
 while [[ $EXT_IP != "$DNS_IP" ]]; do
     sleep 30
-    DNS_IP=$(dig @8.8.8.8 "$RT_HOSTNAME" | grep -v ';' | grep -v CNAME | grep A)
+    DNS_IP=$(dig @8.8.8.8 "$RT_HOSTNAME" | grep -v ';' | grep -v CNAME | grep A | awk '{print $5}')
 done
 
 certbot certonly --standalone -m "$RT_SENDER" --agree-tos --no-eff-email -d "$RT_HOSTNAME","$HOSTNAME" --force-renewal --non-interactive
