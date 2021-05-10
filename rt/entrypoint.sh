224327022115c682b263fd2b207255d34c1d0b44
Peter Reuterås <peter@reuteras.com>
2021-05-10 06:39:51 +0200

    Fix

--- rt/entrypoint.sh
+++ rt/entrypoint.sh
@@ -13,19 +13,21 @@ sed -i -e "s=HOSTNAME=$RT_HOSTNAME=" /tmp/89-rt.conf
 cat /tmp/89-rt.conf > /etc/lighttpd/conf-available/89-rt.conf
 rm -f /tmp/89-rt.conf
 
-cp /etc/msmtprc /tmp/msmtprc
-sed -i -e "s=RT_RELAYHOST=$RT_RELAYHOST=" /tmp/msmtprc
-sed -i -e "s=RT_SENDER=$RT_SENDER=" /tmp/msmtprc
-sed -i -e "s=RT_DOMAIN=$RT_DOMAIN=" /tmp/msmtprc
-cat /tmp/msmtprc > /etc/msmtprc
-rm -f /tmp/msmtprc
+if ! grep "$RT_RELAYHOST" /etc/msmtprc > /dev/null ; then
+    cp /etc/msmtprc /tmp/msmtprc
+    sed -i -e "s=RT_RELAYHOST=$RT_RELAYHOST=" /tmp/msmtprc
+    sed -i -e "s=RT_SENDER=$RT_SENDER=" /tmp/msmtprc
+    sed -i -e "s=RT_DOMAIN=$RT_DOMAIN=" /tmp/msmtprc
+    cat /tmp/msmtprc > /etc/msmtprc
+    rm -f /tmp/msmtprc
+fi
 
 while ! pg_isready -q -h "$RT_DB_HOST" ; do
     echo "Waiting for database on $RT_DB_HOST to be ready."
     sleep 3
 done
 
-if ! PGPASSWORD="$RT_DB_PASS" psql -h "$RT_DB_HOST" -U "$RT_DB_USER" -lqt | cut -d \| -f 1 | grep -qw rtdb ; then
+if ! PGPASSWORD="$RT_DB_PASS" psql -h "$RT_DB_HOST" -U "$RT_DB_USER" -lqt | cut -d \| -f 1 | grep -qw "$RT_DB_NAME" ; then
     echo "Setup database"
     /opt/rt5/sbin/rt-setup-database --dba="$POSTGRES_USER" --dba-password="$POSTGRES_PASSWORD" --action init 
     echo "Database setup done."
