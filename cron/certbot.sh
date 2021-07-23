0e7f2f61c1e07ce7435c65f47acf5238f4f87ebd
Peter Reuterås <peter@reuteras.com>
2021-07-23 17:38:40 +0200

    Update server.pem,

--- cron/certbot.sh
+++ cron/certbot.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
 
 certbot renew
+cat "/etc/letsencrypt/live/$RT_HOSTNAME/cert.pem" "/etc/letsencrypt/live/$RT_HOSTNAME/privkey.pem" > \
+    "/etc/letsencrypt/live/$RT_HOSTNAME/server.pem"
